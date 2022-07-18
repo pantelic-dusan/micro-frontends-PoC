c2eccb6bde7e5204589849d1c1d7ac0033a9299e
pan3bg <pan3bg@bosch.com>
2022-07-18 14:46:43 +0200

    initial mf PoC

---
+++ mf1/src/root.component.tsx
@@ -0,0 +1,3 @@
+export default function Root(props) {
+  return <h1>&nbsp;MICROFRONTEND 1&nbsp;</h1>;;
+}
---
+++ navigation/src/root.component.tsx
@@ -0,0 +1,19 @@
+
+export default function Root(props) {
+  return (
+      <div>
+        <h1>NAVIGATION</h1>
+        <ul className="header">
+          <li><a href="/">Home</a></li>
+          <li><a href="/mf1">MF1</a></li>
+          <li><a href="/mf2">MF2</a></li>
+          <li><a href="/mf1mf2">MF1 & MF2</a></li>
+
+        </ul>
+        <div className="content">
+
+        </div>
+      </div>
+  );
+
+}
